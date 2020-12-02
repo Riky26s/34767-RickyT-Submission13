e75e2d2165e6a4da3ebb8bf1497c2b0083f8fdd2
Riky26s <ricky.t@student.umn.ac.id>
2020-12-03 02:48:41 +0700

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'timerKuis';
+  timeLeft: number = 60;
+  hour: number = 0;
+  minute: number = 0;
+  second: number = 0;
+  interval;
+  timeData = 300;
+
+  hourUp(){
+    this.timeData = this.timeData + 3600;
+  }
+  hourDown(){
+    this.timeData = this.timeData - 3600;
+    if(this.timeData <= 0){
+      this.timeData = 0;
+    }
+  }
+  minuteUp(){
+    this.timeData = this.timeData + 60;
+  }
+  minuteDown(){
+    this.timeData = this.timeData - 60;
+    if(this.timeData <= 0){
+      this.timeData = 0;
+    }
+  }
+  secondUp(){
+    this.timeData = this.timeData + 1;
+  }
+  secondDown(){
+    this.timeData = this.timeData - 1;
+    if(this.timeData <= 0){
+      this.timeData = 0;
+    }
+  }
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 60;
+      }
+    },1000)
+  }
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CountdownModule } from 'ngx-countdown';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CountdownModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
